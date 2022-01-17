235fef67072e879fa00f3c314a4271cf9136f8f8
Vladislav Safarov <egbertz@yandex.ru>
2022-01-17 23:28:52 +0300

    chore: add repositories for main entities

--- src/main/java/ru/inovus/mimimimetr/repository/ContenderRepository.java
+++ src/main/java/ru/inovus/mimimimetr/repository/ContenderRepository.java
@@ -1,4 +1,9 @@
 package ru.inovus.mimimimetr.repository;
 
-public class ContenderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.inovus.mimimimetr.entity.contender.Contender;
+
+@Repository
+public interface ContenderRepository extends JpaRepository<Contender, Long> {
 }
--- src/main/java/ru/inovus/mimimimetr/repository/VoteRepository.java
+++ src/main/java/ru/inovus/mimimimetr/repository/VoteRepository.java
@@ -1,4 +1,9 @@
 package ru.inovus.mimimimetr.repository;
 
-public class VoteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.inovus.mimimimetr.entity.Vote;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
 }
