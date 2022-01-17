a522f8c179802a31449b38d17782916b973ae20e
Vladislav Safarov <egbertz@yandex.ru>
2022-01-17 13:15:42 +0300

    initial commit

---
+++ src/test/java/ru/inovus/mimimimetr/MimimimetrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.inovus.mimimimetr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimimimetrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
