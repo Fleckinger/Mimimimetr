8d8e1358e015aa933b377b70e06aa6e8652b6c2c
Vladislav Safarov <egbertz@yandex.ru>
2022-01-17 23:31:51 +0300

    chore: services for contender and user

--- src/main/java/ru/inovus/mimimimetr/service/ContenderService.java
+++ src/main/java/ru/inovus/mimimimetr/service/ContenderService.java
@@ -1,4 +1,8 @@
 package ru.inovus.mimimimetr.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContenderService {
+
 }
--- src/main/java/ru/inovus/mimimimetr/service/UserService.java
+++ src/main/java/ru/inovus/mimimimetr/service/UserService.java
@@ -1,4 +1,48 @@
 package ru.inovus.mimimimetr.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.inovus.mimimimetr.entity.User;
+import ru.inovus.mimimimetr.repository.UserRepository;
+
+import javax.transaction.Transactional;
+
+
+@Service
+@Transactional
 public class UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    public User registerNewUser(User user) {
+
+        if (emailExists(user.getEmail())) {
+            throw new UserAlreadyExistsException("Данный email занят");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void update(User user) {
+
+    }
+
+    public boolean emailExists(String email) {
+        return userRepository.findByEmail(email).isPresent();
+    }
+
 }
