72cb45cc462ad6c65c3cecae848c9f13d72d34ce
Vladislav Safarov <egbertz@yandex.ru>
2022-01-17 23:35:15 +0300

    feat: add authorization

---
+++ src/main/java/ru/inovus/mimimimetr/controller/LoginController.java
@@ -0,0 +1,16 @@
+package ru.inovus.mimimimetr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage(Principal principal) {
+        return principal == null ? "login": "redirect:note/all";
+    }
+
+}
