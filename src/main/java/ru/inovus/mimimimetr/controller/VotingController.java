a693aa7e8752f6ab23428883d1e5ddb8bd4906fa
Vladislav Safarov <egbertz@yandex.ru>
2022-01-19 12:54:30 +0300

    feat: add mappings for voting page and leaderboard page.
    Voting mapping receive contenders pair and pass them to model, if number of contenders less than two - redirects to the leaderboard page.

--- src/main/java/ru/inovus/mimimimetr/controller/VotingController.java
+++ src/main/java/ru/inovus/mimimimetr/controller/VotingController.java
@@ -1,16 +1,40 @@
 package ru.inovus.mimimimetr.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.inovus.mimimimetr.entity.contender.Contender;
+import ru.inovus.mimimimetr.service.ContenderService;
+
+import java.util.List;
 
 
-@Controller()
+@Controller
 @RequestMapping("/voting")
 public class VotingController {
 
+    private final ContenderService contenderService;
+
+    @Autowired
+    public VotingController(ContenderService contenderService) {
+        this.contenderService = contenderService;
+    }
+
     @GetMapping("")
-    public String getContenders() {
-        return "voting";
+    public String getContenders(Model model) {
+        List<Contender> pair = contenderService.getPair();
+        if (pair.size() != 2) {
+            return "redirect:/voting/leaderboard";
+        }
+        model.addAttribute("pair", contenderService.getPair());
+        return "/voting/voting";
+    }
+
+    @GetMapping("/leaderboard")
+    public String getLeaderboard(Model model) {
+        model.addAttribute("", contenderService.getTop(10));
+        return "/voting/leaderboard";
     }
 }
