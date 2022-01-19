c51c4677dfd569b7268c0923be5ef6437d8ce1d2
Vladislav Safarov <egbertz@yandex.ru>
2022-01-20 01:07:22 +0300

    refactor: remove unused paths in configure method

--- src/main/java/ru/inovus/mimimimetr/config/SecurityConfig.java
+++ src/main/java/ru/inovus/mimimimetr/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package ru.inovus.mimimimetr.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -47,8 +46,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().mvcMatchers("/resources/**", "/static/**", "/css/**", "/images/**");
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/resources/**");
     }
 
     @Bean
