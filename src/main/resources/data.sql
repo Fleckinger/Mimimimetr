eb62c5768f3b79c7e1ffddffa10a20507d965cb5
Vladislav Safarov <egbertz@yandex.ru>
2022-01-19 14:01:03 +0300

    chore: add contender table initializer

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO contender (contender_id, image, name, type)  VALUES
+(1,'1.jpg', 'Соня', 1),
+(2, '2.jpg', 'Бонифаций', 1),
+(3, '3.jpg', 'Абрикос', 1),
+(4, '4.jpg', 'Дуся', 1),
+(5, '5.jpg', 'Дюшес', 1);
