c650ff431d21c0c6d4b15c9db43a1a922d765292
Vladislav Safarov <egbertz@yandex.ru>
2022-01-19 12:43:40 +0300

    feat: add pair selection and top N pair selection from DB

--- src/main/java/ru/inovus/mimimimetr/repository/ContenderRepository.java
+++ src/main/java/ru/inovus/mimimimetr/repository/ContenderRepository.java
@@ -1,9 +1,25 @@
 package ru.inovus.mimimimetr.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import ru.inovus.mimimimetr.entity.User;
 import ru.inovus.mimimimetr.entity.contender.Contender;
+
+import java.util.List;
 
 @Repository
 public interface ContenderRepository extends JpaRepository<Contender, Long> {
+
+    @Query("SELECT c FROM Contender c WHERE c.id = (" +
+            "SELECT vt.contender.id FROM Vote vt WHERE vt.user.id <> :#{#user.getId()}) "
+            )
+    Page<Contender> getContenderWithoutVoteFromUser(@Param("user") User user, Pageable pageable);
+
+    @Query("SELECT c FROM Contender c ORDER BY c.votes.size DESC")
+    List<Contender> findTopByVotes(Pageable pageable);
+
 }
--- src/main/java/ru/inovus/mimimimetr/service/ContenderService.java
+++ src/main/java/ru/inovus/mimimimetr/service/ContenderService.java
@@ -1,8 +1,38 @@
 package ru.inovus.mimimimetr.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import ru.inovus.mimimimetr.entity.User;
+import ru.inovus.mimimimetr.entity.contender.Contender;
+import ru.inovus.mimimimetr.repository.ContenderRepository;
+
+import java.util.List;
 
 @Service
 public class ContenderService {
+
+    private ContenderRepository contenderRepository;
+    private UserService userService;
+
+    @Autowired
+    public ContenderService(ContenderRepository contenderRepository, UserService userService) {
+        this.contenderRepository = contenderRepository;
+        this.userService = userService;
+    }
+
+    public ContenderService() {
+    }
+
+    public List<Contender> getPair() {
+        User currentUser = userService.getCurrentUser();
+
+        return contenderRepository
+                .getContenderWithoutVoteFromUser(currentUser, PageRequest.of(0, 2)).getContent();
+    }
+
+    public List<Contender> getTop(int number) {
+        return contenderRepository.findTopByVotes(PageRequest.of(0, number));
+    }
 
 }
