d9f3fb0f4d0d0014059fbc68adc2b5151b1975bd
Vladislav Safarov <egbertz@yandex.ru>
2022-01-20 01:09:04 +0300

    feat: add method that saves new vote

--- src/main/java/ru/inovus/mimimimetr/controller/VotingController.java
+++ src/main/java/ru/inovus/mimimimetr/controller/VotingController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.inovus.mimimimetr.entity.Vote;
 import ru.inovus.mimimimetr.entity.contender.Contender;
 import ru.inovus.mimimimetr.service.ContenderService;
+import ru.inovus.mimimimetr.service.UserService;
 
 import java.util.List;
 
@@ -16,10 +20,12 @@ import java.util.List;
 public class VotingController {
 
     private final ContenderService contenderService;
+    private final UserService userService;
 
     @Autowired
-    public VotingController(ContenderService contenderService) {
+    public VotingController(ContenderService contenderService, UserService userService) {
         this.contenderService = contenderService;
+        this.userService = userService;
     }
 
     @GetMapping("")
@@ -32,9 +38,20 @@ public class VotingController {
         return "/voting/voting";
     }
 
+    @PostMapping("")
+    public String vote(@RequestParam("id") Long contenderId) {
+
+        Vote vote = new Vote();
+        vote.setContender(contenderService.findById(contenderId).orElseThrow());
+        vote.setUser(userService.getCurrentUser());
+        contenderService.saveVote(vote);
+
+        return "redirect:/voting";
+    }
+
     @GetMapping("/leaderboard")
     public String getLeaderboard(Model model) {
-        model.addAttribute("", contenderService.getTop(10));
+        model.addAttribute("top", contenderService.getTop(10));
         return "/voting/leaderboard";
     }
 }
