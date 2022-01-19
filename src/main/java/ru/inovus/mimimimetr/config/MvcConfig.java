77c9396c84b1d6bb8ce9d9a485a3dddc4f70efa8
Vladislav Safarov <egbertz@yandex.ru>
2022-01-20 01:06:37 +0300

    refactor: remove @EnableWebMvc because I'm fine with the default settings

--- src/main/java/ru/inovus/mimimimetr/config/MvcConfig.java
+++ src/main/java/ru/inovus/mimimimetr/config/MvcConfig.java
@@ -1,12 +1,12 @@
 package ru.inovus.mimimimetr.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@EnableWebMvc
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
